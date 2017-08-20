adb3ef56501cadb85a9ace1496e54a7047578e90
prvnk10 <prvnk10@gmail.com>
2017-08-20 12:43:55 +0530

    Add files via upload

---
+++ templates/footer.php
@@ -0,0 +1,9 @@
+<div id="footer">
+  <a href="./about.php" target="_blank"> About Us | <a href="./feedback.php" target="_blank"> Feedback </a> |
+  <a href="./terms-and-conditions.php" target="_blank"> Terms and Conditions </a> | <a href="./get-in-touch.php" target="_blank"> Get in touch </a>
+</div>
+
+</div>   <!-- this closes the div(in index.php file) whose class is container -->
+
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link href="./styles/bootstrap.css" rel="stylesheet" type="text/css"> </link>
+<script src="./jquery-3.1.0.js"> </script>
+
+<!--
+  <link href="./styles/index.css" rel="stylesheet" type="text/css">
+  <link href="./styles/style.css" rel="stylesheet" type="text/css">
+  <link href="./styles/forms.css" rel="stylesheet" type="text/css">
+  <link href="./styles/bootstrap.css" rel="stylesheet" type="text/css">
+  <script src="./require/jquery-3.1.0.js"> </script>
+  <script src="./require/hover.js" type="text/javascript"> </script>
+-->
+
+</head>
+
+<title> <?php echo htmlspecialchars($title); ?> </title>
+
+<body>
+
+<!-- <div id="header"> -->
+
+<div class="container">
+  <div class="page-header">
+
+  <!-- <div class="site_name_and_logo"> -->
+
+  <h2>
+    <a href="./"> <img src="./require/logo.png" alt="site_logo" title="logo" class="logo" /> </a>
+    <a href="./"> Knowledge is power </a>
+  </h2>
